2bb04e4a7c4e1b0702d50c67ed674e507b6cc10f
Vartika Kesarwani <vartika.conaxweb@gmail.com>
2022-03-30 12:48:40 +0530

    role update

--- routes/v1/role/index.js
+++ routes/v1/role/index.js
@@ -6,6 +6,7 @@ const  RoleValidator=require('../../../middlewares/validation/roleValidation');
 router.get('/',RoleController.listAll);
 router.get('/search',RoleController.search);
 router.get('/:role_id',RoleController.view);
+router.get('/:name',RoleController.view);
 router.post('/',RoleValidator.create,RoleController.create);
 router.put('/:role_id',RoleValidator.update,RoleController.update);
 router.delete('/:role_id',RoleController.destroy);
